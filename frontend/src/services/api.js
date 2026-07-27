32aca37922413907825f1b1889bd40bfc783d809
Kritik Kaushik <kritikkaushik2004@gmail.com>
2026-07-28 00:16:23 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://ebook-library-d3kg.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {

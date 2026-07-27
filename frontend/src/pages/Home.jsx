f2f3e25d778c0861cae15eb115c84188f34de6ad
Kritik Kaushik <kritikkaushik2004@gmail.com>
2026-07-28 00:35:18 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -116,7 +116,7 @@ function Home() {
             <div key={book._id} className="book-item">
               {book.cover && (
                 <img
-                  src={`http://localhost:8000${book.cover}`}
+                  src={`https://ebook-library-d3kg.onrender.com${book.cover}`}
                   alt={book.title}
                 />
               )}

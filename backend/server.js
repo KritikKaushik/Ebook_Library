58349aca2c4ccb9eff589802a542a57feb595313
Kritik Kaushik <kritikkaushik2004@gmail.com>
2026-07-28 00:28:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://ebook-library-gamma.vercel.app",
+    ],
     credentials: true,
   })
 );
